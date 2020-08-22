4d09873b1e6b80b3617b12864f09288042491bd0
Stuart P. Bentley <s@stuartpb.com>
2020-08-22 00:56:40 -0700

    Add license to userscript

--- corexit.user.js
+++ corexit.user.js
@@ -2,12 +2,40 @@
 // @name         Corexit
 // @version      0.2.0
 // @description  Clean up a disaster
-// @author  Stuart P. Bentley <stuart@testtrack4.com> (https://stuartpb.com)
+// @author  Stuart P. Bentley <s@stuartpb.com> (https://stuartpb.com)
+// @license      sandia-wtfpl
 // @match        *://*/*
 // @run-at       document-start
 // @grant        GM_getValue
 // @grant        GM_setValue
 // ==/UserScript==
+
+/*
+Copyright 2016 Stuart P. Bentley
+
+⚠️ WARNING! ⚠️  
+☢️ 😱 DO NOT USE THIS PROGRAM. 😱 ☢️  
+This program is not a program of honor.  
+
+No highly esteemed function is executed here.  
+
+What is here is dangerous and repulsive to us.  
+
+The danger is still present, in your time, as it was in ours,  
+without even the implied warranty of MERCHANTABILITY or  
+FITNESS FOR A PARTICULAR PURPOSE.
+
+This program is best shunned and left unused (but it is free software,  
+and you are welcome to redistribute it under certain conditions).  
+😱 ☢️ DO NOT USE THIS PROGRAM. ☢️ 😱
+
+This program is licensed under the Sandia Message Public License,  
+sublicense Do What The Fuck You Want To Public License.  
+This may be abbreviated as sandia-wtfpl.  
+You may obtain a copy of the License(s) at  
+https://github.com/cdanis/sandia-public-license/blob/main/LICENSE.md and  
+https://spdx.org/licenses/WTFPL.html
+*/
 
 /* global GM_getValue GM_setValue location */
 
